d31064c8c06fd0ea3945bcd73a163335d2b05141
dopehunter <2k.krasnov@gmail.com>
2025-01-24 21:54:50 +0100

    Update banks_project.py: Modify CSV paths for data extraction and output, enhance transformation function to include multiple currency conversions, and streamline database loading process. Improve logging and error handling in query execution.

--- banks_project.py
+++ banks_project.py
@@ -17,7 +17,8 @@ conn = sqlite3.connect('Banks.db')
 table_name = 'Largest_banks'
 table_attribs_initial = ['Name', 'MC_USD_Billion']
 table_attribs_final = ['Name', 'MC_USD_Billion', 'MC_GBP_Billion', 'MC_EUR_Billion', 'MC_INR_Billion']
-csv_path = './Largest_banks_data.csv'
+csv_path = './exchange_rate.csv'
+output_path = './Largest_banks_data.csv'
 log_file = './log.txt'
 
 
@@ -31,8 +32,7 @@ def log_progress(message):
     with open(log_file, 'a') as f:
         f.write(timestamp + ' : ' + message + '\n')
     return message
-    
-    
+     
 
 def extract(url):
     ''' This function aims to extract the required
@@ -47,16 +47,17 @@ def extract(url):
     print(log_progress(f"Number of tables fetched: {len(tables)}"))
     rows = tables[0].find_all('tr') # returns a list of all the rows in the table
     print(log_progress(f"Number of rows defined: {len(rows)}"))
+    df = pd.DataFrame()
     for row in rows:
         columns = row.find_all('td') # returns a list of all the columns in the row
-        if columns:
+        if columns: # checks if the row has columns
             bank_name = columns[1].text.strip()
             try:
                 market_cap = float(columns[2].text.strip())
             except ValueError:
                 market_cap = float('inf')
             
-            df = pd.DataFrame({'Name': [bank_name], 'MC_USD_Billion': [market_cap]})
+            df = pd.concat([df, pd.DataFrame({'Name': [bank_name], 'MC_USD_Billion': [market_cap]})], ignore_index=True)
     print(log_progress('Data extraction complete. Initiating Transformation process'))
     return df
 
@@ -68,35 +69,63 @@ def transform(df, csv_path):
     containing the transformed version of Market Cap column to
     respective currencies'''
     
-    
-    print(log_progress('Transformation process complete. Initiating Load process'))
+    exchange_rates = pd.read_csv(csv_path) # returns a data frame with exchange rates
+    print(log_progress('Exchange rates fetched'))
+    dict_exchange_rates = exchange_rates.set_index('Currency').to_dict()['Rate']
+    df['MC_GBP_Billion'] = [np.round(x * dict_exchange_rates['GBP'],2) for x in df['MC_USD_Billion']]
+    df['MC_EUR_Billion'] = [np.round(x * dict_exchange_rates['EUR'],2) for x in df['MC_USD_Billion']]
+    df['MC_INR_Billion'] = [np.round(x * dict_exchange_rates['INR'],2) for x in df['MC_USD_Billion']]
+    print(log_progress('Data transformation complete. Initiating Loading process'))
     return df
 
 def load_to_csv(df, output_path):
     ''' This function saves the final data frame as a CSV file in
     the provided path. Function returns nothing.'''
-
+    df.to_csv(output_path, index=False)
     print(log_progress('Data saved to CSV file'))
 
-def load_to_db(df, sql_connection, table_name):
+def load_to_db(df, conn, table_name):
     ''' This function saves the final data frame to a database
     table with the provided name. Function returns nothing.'''
-    
     print(log_progress('SQL Connection initiated'))
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
+    conn.commit()
     print(log_progress('Data loaded to Database as a table, Executing queries'))
 
 
-def run_query(query_statement, sql_connection):
+def run_query(query_statement, conn):
     ''' This function runs the query on the database table and
     prints the output on the terminal. Function returns nothing. '''
-
-    print(log_progress('Process Complete'))
-    print(log_progress('Server Connection closed'))
+    conn = sqlite3.connect('Banks.db')
+    try:
+            # Execute the query and fetch results
+            cursor = conn.cursor()
+            cursor.execute(query_statement)
+            results = cursor.fetchall()
+            
+            # Print results
+            for row in results:
+                print(row)
+                
+    except sqlite3.Error as e:
+        print(f"Error executing query: {e}")
+    conn.close()
+    print(log_progress('\nProcess Complete \n'))
+    
 
 
 ''' Here, you define the required entities and call the relevant
 functions in the correct order to complete the project. Note that this
 portion is not inside any function.'''
 
-extract(url)
+df = extract(url)
+df = transform(df, csv_path)
+
+load_to_db(df, conn, table_name)
+print('\nAll largest banks:')
+run_query('SELECT * FROM Largest_banks', conn)
+print('Average market cap in GBP:')
+run_query('SELECT AVG(MC_GBP_Billion) FROM Largest_banks', conn)
+print('First 5 largest banks:')
+run_query('SELECT Name from Largest_banks LIMIT 5', conn)
 
