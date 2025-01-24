1f5baf4e6ea7ec2a634c9be0a04cb0f127addb7e
dopehunter <2k.krasnov@gmail.com>
2025-01-24 18:28:03 +0100

    Enhance ETL process in banks_project.py: Added functions for extraction, transformation, and loading of data; improved logging for each stage of the process; updated URL for bank data source.

--- banks_project.py
+++ banks_project.py
@@ -1,3 +1,7 @@
+# Code for ETL operations on Country-GDP data
+
+# Importing the required libraries
+
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
@@ -6,19 +10,72 @@ import logging
 import sqlite3
 from datetime import datetime
 
-url = "https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks"
+url = "https://en.wikipedia.org/wiki/List_of_largest_banks"
 exchange_rates_url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-PY0221EN-Coursera/labs/v2/exchange_rate.csv"
 
 conn = sqlite3.connect('Banks.db')
 table_name = 'Largest_banks'
 table_attribs_initial = ['Name', 'MC_USD_Billion']
 table_attribs_final = ['Name', 'MC_USD_Billion', 'MC_GBP_Billion', 'MC_EUR_Billion', 'MC_INR_Billion']
-output_csv_path = './Largest_banks_data.csv'
+csv_path = './Largest_banks_data.csv'
 log_file = './log.txt'
 
+
+
 def log_progress(message):
+    ''' This function logs the mentioned message of a given stage of the
+    code execution to a log file. Function returns nothing'''
     timestamp_format = '%Y-%h-%d %H:%M:%S,%f'
     now = datetime.now()
     timestamp = now.strftime(timestamp_format)
     with open(log_file, 'a') as f:
         f.write(timestamp + ' : ' + message + '\n')
+    return message
+    
+print(log_progress('Preliminaries complete. Initiating ETL process'))    
+
+def extract(url, table_attribs_initial):
+    ''' This function aims to extract the required
+    information from the website and save it to a data frame. The
+    function returns the data frame for further processing. '''
+    
+    
+    print(log_progress('Data extraction complete. Initiating Transformation process'))
+    return df
+
+
+def transform(df, csv_path):
+    ''' This function accesses the CSV file for exchange rate
+    information, and adds three columns to the data frame, each
+    containing the transformed version of Market Cap column to
+    respective currencies'''
+    
+    
+    print(log_progress('Transformation process complete. Initiating Load process'))
+    return df
+
+def load_to_csv(df, output_path):
+    ''' This function saves the final data frame as a CSV file in
+    the provided path. Function returns nothing.'''
+
+    print(log_progress('Data saved to CSV file'))
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final data frame to a database
+    table with the provided name. Function returns nothing.'''
+    
+    print(log_progress('SQL Connection initiated'))
+    print(log_progress('Data loaded to Database as a table, Executing queries'))
+
+
+def run_query(query_statement, sql_connection):
+    ''' This function runs the query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+
+    print(log_progress('Process Complete'))
+    print(log_progress('Server Connection closed'))
+
+
+''' Here, you define the required entities and call the relevant
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
